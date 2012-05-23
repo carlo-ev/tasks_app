595d2411aa0b04abbf487fd45a5b5aec007d197a
Carlo Espinal <carlo_ev@unitec.edu>
2012-05-23 11:35:45 -0600

    scaffold users, sessions controller, log system check needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+	if sesssion[:user_id]
+		@current_user ||= User.find(session[:user_new])
+  end
+
+  def logged_in?
+	current_user.present?
+  end
+  helper_method :current_user, :logged_in?
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Tasks::Application.routes.draw do
-  resources :users
 
-resources :tasks
-root to: "tasks#index"
+  resources :users
+  resources :sessions
+  resources :tasks
+
+  root to: "tasks#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
